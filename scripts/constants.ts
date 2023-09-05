2c4371cb22789f172a0b8ba3a7effa5804e6a51c
Marco Jardim <marcoeojardim@gmail.com>
2023-09-05 09:31:37 -0300

    Adding constants.ts

---
+++ scripts/constants.ts
@@ -0,0 +1,11 @@
+export const CONTRACTS: Record<string, string> = {
+  gigaToken: "GigaToken",
+  multisig: "MultiSig",
+  verifier: "Verifier",
+};
+
+export const CONFIGURATION: Record<string, any> = {
+  hardhatChainId: 1337,
+  secondSignerAddress: "0x5555581E88072387CDBdBb5732Ec58B23564D40f",
+  thirdSignerAddress: "0x7777798435A15E461589Ff3A0D29ac44E17DD039"
+};\
